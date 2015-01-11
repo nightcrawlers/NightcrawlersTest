dad6fba119953e1432e59d7dddc84791ba47b1cc
Larantz <Larantz@users.noreply.github.com>
2015-01-12 00:31:12 +0100

    testing to create new team solution.

---
+++ ClassLibraryTest/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTest
+{
+    public class HelloWorld
+    {
+        pulic string DoYourThing()
+        {return "Hello world!";}
+    }
+}
